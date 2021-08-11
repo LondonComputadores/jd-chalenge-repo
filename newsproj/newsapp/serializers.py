9e57daec9babc56a2a9253befed0a03e329c0699
LondonComputadores <london.computadores@gmail.com>
2021-08-11 19:38:18 -0400

    NEWSAPP AND ACCOUNTS: Created these 2 apps, configured and tested them up, working okay but still need the right api endpoints as per required

---
+++ newsproj/newsapp/serializers.py
@@ -0,0 +1,9 @@
+# from rest_framework import serializers
+# from court_app.models import ClassModel #change this for the class \
+                                          # from models
+
+# class ClassModelSerializer(serializers...):
+#    """Allow to choose only the fields that will be visible on API"""
+    # class Meta:
+    #     model = ClassModel
+    #     fields = ['', '',...]
